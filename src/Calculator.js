19fa6280a189d954493932b07054c98f6122854b
Yogesh070 <ythapa430@gmail.com>
2021-09-19 19:25:33 +0545

    added more functionality and fixed layout

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,10 +2,11 @@ import {React, useState} from 'react'
 import Key from './Key'
 import './Calculator.css'
 function Calculator() {
-    const nums=[1,2,3,4,5,6,7,8,9,0];
+    const nums=[1,2,3,4,5,6,7,8,9];
     const operationKeys=['/','*','-','+'];
+    const operations=['/','*','-','+','%','.'];
     const [screenDigit, setscreenDigit] = useState('0');
-    const [historyLog, sethistoryLog] = useState(['3*2']);
+    const [historyLog, sethistoryLog] = useState([]);
 
     const onButtonClick=(button) =>{  
         if (button==='.' && screenDigit.includes('.')) {
@@ -23,7 +24,7 @@ function Calculator() {
         const setbool=(e)=>{
         if(screenDigit.endsWith(e)){ checker=true}
         }
-        operationKeys.forEach(setbool);
+        operations.forEach(setbool);
         if (checker) return
         setscreenDigit(screenDigit + operationKey);
     }
@@ -36,25 +37,63 @@ function Calculator() {
       }
       
     const calulateResult=()=>{
+        if (screenDigit.length===1) return
         let result =evil(screenDigit);
-        setscreenDigit(result.toString())
+        setscreenDigit(result.toFixed(3).toString())
         sethistoryLog([...historyLog,`${screenDigit}=${result}`])
+    }
+    const randomNum=()=>{
+        let randomNum=Math.random()*10;
+        setscreenDigit(screenDigit + Math.floor(randomNum));
+    }
+    const backSpace=()=>{
+        console.log('hi');
+        console.log(screenDigit.substring(0,screenDigit.length-1));
+        setscreenDigit(prev=>prev.substring(0, screenDigit.length - 1));
+    }
+    const generateRandomColor=()=>{
+        let randomColor = Math.floor(Math.random()*16777215).toString(16);
+        return `#${randomColor}`;
     }
     return (
         <div className="calculator">
-            <div className="screen">
-                <h4>{historyLog[historyLog.length-1]}</h4>
-                <h2 style={{fontSize:'50px'}}>{screenDigit}</h2>
+            <div className="history-log">
+                {historyLog.reverse().map(history=><h2 style={{color:generateRandomColor()}}>{history}</h2>)}
             </div>
-            <div className="keyboard">
-                <div className="numbers-key">
-                    {nums.reverse().map(num=><Key key={num}  keyboardKey={num} onTap={()=>onButtonClick(num)}/>)}
-                    {operationKeys.map(operationKey=><Key key={operationKey} onTap={()=>onOperationButtonClick(operationKey)} color='#D26C6F' keyboardKey={operationKey}/> )}
+            <div className="calculator-body">
+                <div className="screen">
+                    <h4>{historyLog[historyLog.length-1]}</h4>
+                    <div className="result" style={{wordWrap:"break-word"}}>
+                    <h2 style={{fontSize:'50px'}}>{screenDigit}</h2>
+                    </div>
                 </div>
-                <Key keyboardKey='AC' onTap={clearScreen}/>
-                <Key keyboardKey='.' onTap={()=>onButtonClick('.')}/>
-                <Key keyboardKey='=' onTap={calulateResult}/>
+                <div className="keyboard">
+                    <div className="top-pad">
+                    <Key keyboardKey='AC' onTap={clearScreen} color='#229A89' />
+                    <Key keyboardKey='%' onTap={()=>onOperationButtonClick('%')} color='#229A89' />
+                    <Key keyboardKey={'Random'} onTap={randomNum}/>
+
+                    </div>
+                    <div className="numbers-key">
+                        <div className="numbers-key-grid">
+                            {nums.reverse().map(num=><Key key={num}  keyboardKey={num} onTap={()=>onButtonClick(num)}/>)}
+                        </div>
+                        
+                    </div>
+                    <div className="operation-key">
+                        {operationKeys.map(operationKey=><Key key={operationKey} onTap={()=>onOperationButtonClick(operationKey)} color='#D26C6F' keyboardKey={operationKey}/> )}
+                        <Key keyboardKey='=' onTap={calulateResult}/>
+                    </div>
+                    <div className="bottom-pad">
+                        <div className="bottom-pad-grid">
+                        <Key keyboardKey='back' onTap={backSpace}/>
+                        <Key keyboardKey='0' onTap={()=>onButtonClick('0')}/>
+                        <Key keyboardKey='.' onTap={()=>onButtonClick('.')}/></div>
+                        
+                    </div>
+                </div>
             </div>
+            
         </div>
     )
 }
